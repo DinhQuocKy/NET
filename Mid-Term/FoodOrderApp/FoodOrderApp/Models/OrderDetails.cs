a654bf5b2d9e094ab6f6e0e5ec81b46d8218d458
= <=>
2019-09-19 20:34:25 +0700

    Write API for Order

---
+++ Mid-Term/FoodOrderApp/FoodOrderApp/Models/OrderDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodOrderApp.Models
+{
+    public partial class OrderDetails
+    {
+        public int DetailId { get; set; }
+        public int OrderId { get; set; }
+        public int FoodId { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Foods Food { get; set; }
+        public virtual Orders Order { get; set; }
+    }
+}
