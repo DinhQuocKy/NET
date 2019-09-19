912f6fec358edb68c385e2aba665e8ae9dab6fd8
= <=>
2019-09-19 20:51:35 +0700

    Finish API Order

--- Mid-Term/FoodOrderApp/FoodOrderApp/Startup.cs
+++ Mid-Term/FoodOrderApp/FoodOrderApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodOrderApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +28,7 @@ namespace FoodOrderApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            //services.AddDbContext<FoodOrderContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FoodOrder")));
+            services.AddDbContext<FoodOrderContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FoodOrder")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
